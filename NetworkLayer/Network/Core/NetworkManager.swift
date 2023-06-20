3d074f3dcfa4098acabeb24f0a3cb3d53fe65490
baris dilekci <bdilekci200@gmail.com>
2023-06-20 16:13:55 +0300

    feat: create network manager

--- NetworkLayer/Network/Core/NetworkManager.swift
+++ NetworkLayer/Network/Core/NetworkManager.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    private init() {}
+    private func request<T:Decodable>(_ endpoint: EndPoint, completion: @escaping (Swift.Result<T,Error>) -> Void) {
+        let task = URLSession.shared.dataTask(with: endpoint.request()) { data , response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode >= 200, response.statusCode <= 299 else {
+                completion(.failure(NSError(domain: "Invalid response", code: 0)))
+                return
+            }
+                           
+            guard let data = data else {
+                completion(.failure(NSError(domain: "Invalid response data", code: 0)))
+                return
+            }
+            
+            do {
+                let decodeData = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodeData))
+            } catch let error {
+                completion(.failure(error))
+            }
+            
+        }
+        task.resume()
+    }
+    
+    func getUser(completion: @escaping (Swift.Result<[User],Error>) -> Void) {
+        let endpoint = EndPoint.getUsers
+        request(endpoint, completion: completion)
+    }
+}
